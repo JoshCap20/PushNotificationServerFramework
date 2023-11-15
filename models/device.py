19136338ff71f33a9d95a983609d7b24cad5ea28
Josh Caponigro <joshcap@email.unc.edu>
2023-11-15 02:03:35 -0500

    For device, make every field but token optional

--- models/device.py
+++ models/device.py
@@ -8,7 +8,7 @@ class Device(BaseModel):
 
     Attributes:
         id (int | None): The unique identifier for the device.
-        token (str): The device token used for push notifications.
+        token (str): The device token used for push notifications. Required.
         name (str): The name of the device.
         systemName (str): The name of the operating system running on the device.
         systemVersion (str): The version of the operating system running on the device.
@@ -20,10 +20,10 @@ class Device(BaseModel):
 
     id: int | None = None
     token: str
-    name: str
-    systemName: str
-    systemVersion: str
-    model: str
-    localizedModel: str
+    name: str | None = None
+    systemName: str | None = None
+    systemVersion: str | None = None
+    model: str | None = None
+    localizedModel: str | None = None
     created_at: datetime | None = None
     updated_at: datetime | None = None
